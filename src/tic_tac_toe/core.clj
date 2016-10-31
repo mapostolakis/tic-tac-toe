632515232b6e2ddc57334f1af82dd119cb7e0f02
Mike Apostolakis <mr.michaela@gmail.com>
2016-10-31 20:56:12 +0200

    Refactoring

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -3,30 +3,48 @@
 (def empty "e")
 (def incomplete "incomplete")
 (def draw "draw")
-
-(defn- keys-for-empty
-  [board]
-  (set (filter (comp #{empty} board) (keys board))))
-
 (def winners [[0 1 2] [3 4 5] [6 7 8]
               [0 3 6] [1 4 7] [2 5 8]
               [2 4 6] [0 4 8]])
+
+(defn- select-values
+  [map keys]
+  (->> [map keys]
+       (apply select-keys)
+       (vals)))
+
+(defn- keys-for-empty
+  [board]
+  (->> board
+       (keys)
+       (filter (comp #{empty} board))
+       (set)))
 
 (defn- contains-empty?
   [board]
   (contains? (set (vals board)) empty))
 
+(defn- hit-winner-combo?
+  [board player combo]
+  (every? #(= player %) (select-values board combo)))
+
+(defn- winners-result
+  [board player]
+  (set (flatten (for [combo winners
+                      :when (hit-winner-combo? board player combo)]
+                  combo))))
+
+(defn- final-result
+  [board player]
+  [player (winners-result board player)])
+
+(defn- won-game?
+  [board player]
+  (not-empty (winners-result board player)))
+
 (defn score-game
   [board, player]
   (cond
-    (= (board 0) (board 1) (board 2) player) [player #{0 1 2}]
-    (= (board 3) (board 4) (board 5) player) [player #{3 4 5}]
-    (= (board 6) (board 7) (board 8) player) [player #{6 7 8}]
-    (= (board 0) (board 3) (board 6) player) [player #{0 3 6}]
-    (= (board 1) (board 4) (board 7) player) [player #{1 4 7}]
-    (= (board 2) (board 5) (board 8) player) [player #{2 5 8}]
-    (= (board 2) (board 4) (board 6) player) [player #{2 4 6}]
-    (= (board 0) (board 4) (board 8) player) [player #{0 4 8}]
+    (won-game? board player) (final-result board player)
     (contains-empty? board) [incomplete (keys-for-empty board)]
-    :else [draw #{}])
-  )
+    :else [draw #{}]))
